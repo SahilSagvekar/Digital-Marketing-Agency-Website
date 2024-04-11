78423445647e5f80ce9affc81ac3979d3c803404
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2024-04-11 22:20:25 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+      domains: ["flowbite.s3.amazonaws.com", 'images.unsplash.com'],
+    },
+  };
 export default nextConfig;
